9452399799e1e1354ff2129945d77ef072bda966
Henrique Degrecci <rickdegrecci@hotmail.com>
2015-11-10 10:06:38 -0200

    Idéia de nova tabela

--- concessionaria.sql
+++ concessionaria.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Cliente
 );
 CREATE TABLE IF NOT EXISTS  Vendedor
 (
-  cd_vendedor serial,
+  cd_vendedor SERIAL,
   vend_nome VARCHAR(12) NOT NULL,
   vend_sobrenome VARCHAR (20),
   vend_sexo CHAR(1) NOT NULL,
@@ -36,6 +36,20 @@ CREATE TABLE IF NOT EXISTS  Vendedor
   vend_dt_admissao DATE,
   vend_fone VARCHAR(13),
   CONSTRAINT Vendedor_pkey PRIMARY KEY (cd_vendedor)
+);
+CREATE TABLE IF NOT EXISTS  Endereco
+(
+  cd_endereco SERIAL,
+  rua VARCHAR(20) NOT NULL,
+  numero VARCHAR(5) NOT NULL, /* Coloquei em varchar porque tem nºs como 17-A e 17-B */
+  complemento VARCHAR(20) NOT NULL,
+  bairro VARCHAR(12) NOT NULL,
+  cidade VARCHAR(12) NOT NULL,
+  estado VARCHAR(10) NOT NULL,
+  naturalidade VARCHAR(12) NOT NULL,
+  vendedor_fk char(6) NOT NULL REFERENCES Vendedor(cd_vendedor)ON UPDATE CASCADE,
+  cliente_fk char(6) NOT NULL REFERENCES Cliente(cd_cliente)ON UPDATE CASCADE,
+  CONSTRAINT Endereco_pkey PRIMARY KEY (cd_endereco)
 );
 CREATE TABLE IF NOT EXISTS Venda
 (

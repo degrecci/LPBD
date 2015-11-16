a97d7d2d624c4cf6bca104193cba0ac3a9387f5b
Sérgio Praxedes <rock.praxedes@yahoo.com.br>
2015-11-16 11:30:51 -0200

    FINISH
    tabela completa!

--- concessionaria.sql
+++ concessionaria.sql
@@ -1,7 +1,3 @@
-/*CREATE DATABASE CONCESSIONARIA;
-CREATE USER gerente with PASSWORD 'senha';
-GRANT ALL PRIVILEGES ON CONCESSIONARIA TO gerente;
-*/
 
 /* CRIAÇÃO DAS TABELAS -- EXECUTAR PRIMEIRO */
 CREATE TABLE IF NOT EXISTS Loja
@@ -85,20 +81,16 @@ BEGIN
 		SELECT vendedor_fk
 		FROM Venda
 		ORDER BY cd_venda DESC
-		LIMIT (1));
-	DELETE FROM CARRO
-	WHERE cd_carro = (
-		SELECT carro_fk
-		FROM Venda
-		ORDER BY cd_venda DESC
-		limit (1)
-	); return old
-	END;
+		LIMIT (1));return new;
+END;
 $VENDAS$ LANGUAGE plpgsql;
 
 
 CREATE TRIGGER VENDAS AFTER INSERT ON Venda
 FOR EACH ROW EXECUTE PROCEDURE VENDEU();
+
+
+
 
 
 /* INSERÇÃO DE VALORES -- EXECUTAR EM QUARTO */
@@ -138,32 +130,47 @@ VALUES ('MITSUBISHI','ECLIPSE',150.00,'PRETO',2015,'NOVO','9BM48503826482467',07
 INSERT INTO Vendedor (vend_nome,vend_sobrenome,vend_sexo,vend_salario,vend_vendidos,vend_admissao,Loja_fk)
 VALUES ('Juca','Neves Monteiro','M',2300,5,'04-05-2010',13);
 INSERT INTO Vendedor (vend_nome,vend_sobrenome,vend_sexo,vend_salario,vend_vendidos,vend_admissao,Loja_fk)
-VALUES ('Lucas','Silva e Silva','M',2000,1,'18-07-2013',07);
+VALUES ('Lucas','Silva e Silva','M',2000,1,'10-22-2013',07);
 INSERT INTO Vendedor (vend_nome,vend_sobrenome,vend_sexo,vend_salario,vend_vendidos,vend_admissao,Loja_fk)
-VALUES ('Mario','Carneiro Pedregulho','M',2200,2,'28-01-2011',55);
+VALUES ('Mario','Carneiro Pedregulho','M',2200,2,'02-01-2011',55);
 INSERT INTO Vendedor (vend_nome,vend_sobrenome,vend_sexo,vend_salario,vend_vendidos,vend_admissao,Loja_fk)
-VALUES ('Marina','Chaves Bim','F',3400,16,'24-12-2012',07);
+VALUES ('Marina','Chaves Bim','F',3400,16,'08-30-2012',07);
 INSERT INTO Vendedor (vend_nome,vend_sobrenome,vend_sexo,vend_salario,vend_vendidos,vend_admissao,Loja_fk)
-VALUES ('Cacilda','Flavia Moreno','F',1400,0,'28-10-2015',99);
+VALUES ('Cacilda','Flavia Moreno','F',1400,0,'12-28-2015',99);
 
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Claudio','Gilberto Santos','17-09-1990','m',35498769846,19564688,357098504,78);
+VALUES ('Claudio','Gilberto Santos','11-09-1990','m',35498769846,19564688,357098504,78);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Maria','Hortela Carla Milda','21-04-1981','f',36597452146,1923356548,13055698,55);
+VALUES ('Maria','Hortela Carla Milda','11-04-1981','f',36597452146,1923356548,13055698,55);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Amelia','Gilba Junk','17-09-1970','f',32698564875,1999995745,65325698,258);
+VALUES ('Amelia','Gilba Junk','10-09-1970','f',32698564875,1999995745,65325698,258);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Claudia','Harrison Ford','17-09-1984','f',64573820956,1132699658,65874512,666);
+VALUES ('Claudia','Harrison Ford','03-09-1984','f',64573820956,1132699658,65874512,666);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Francisca','Helena Marques','17-09-1994','f',84569530548,1398854682,245987524,01);
+VALUES ('Francisca','Helena Marques','04-09-1994','f',84569530548,1398854682,245987524,01);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Joaquim,','Junior Filho Neto','17-09-19','m',956832609886,14659896,34678465,678);
+VALUES ('Joaquim,','Junior Filho Neto','07-09-1976','m',956832609886,14659896,34678465,678);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Augusto','Garoto Fru','17-09-1990','m',41387456993,19986593,16798740,77789);
+VALUES ('Augusto','Garoto Fru','11-04-1990','m',41387456993,19986593,16798740,77789);
 INSERT INTO Cliente(nome,sobrenome,nasc,sexo,cpf,fone,cep,nr)
-VALUES ('Victor','Silva Peres','17-09-1990','m',21695303669,1132659974,14659896,6458);
+VALUES ('Victor','Silva Peres','01-09-1990','m',21695303669,1132659974,14659896,6458);
 
-/* COMANDOS DE SELEÇÃO -- EXECUTAR EM QUINTO */
+/* INSERÇÃO DE VALORES 2, EXECUTA A TRIGGER, EXECUTAR EM quinto */
+
+
+INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
+VALUES (7,13,'12-05-2010',5,5)
+INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
+VALUES (1,55,'06-05-2013',1,2)
+INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
+VALUES (3,01,'05-04-2012',2,1)
+INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
+VALUES (5,07,'11-07-2011',4,3)
+INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
+VALUES (8,99,'02-12-2014',3,4)
+
+
+/* COMANDOS DE SELEÇÃO -- EXECUTAR EM sexto */
 
 SELECT marca, modelo, ano, cor, estado, valor
 FROM carro
@@ -176,17 +183,6 @@ ORDER BY vend_salario DESC;
 SELECT v.vend_nome, v.vend_sobrenome, v.vend_vendidos, l.lj_nome
 FROM Vendedor as v INNER JOIN Loja as l on l.cd_loja = v.cd_vendedor;
 
-/* INSERÇÃO DE VALORES 2, EXECUTA A TRIGGER, EXECUTAR EM SEXTO */
 
-/*
-INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
-VALUES (7,13,'22-05-2010',5,5)
-INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
-VALUES (1,55,'06-05-2013',1,2)
-INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
-VALUES (3,01,'05-04-2012',2,1)
-INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
-VALUES (5,07,'25-07-2011',4,3)
-INSERT INTO Venda(carro_fk,loja_fk,venda_data,cliente_fk,vendedor_fk)
-VALUES (8,99,'30-12-2014',3,4)
-*/
+
+
